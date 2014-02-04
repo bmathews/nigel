36466ac2a56d8632990fa5aa7b576e90f494d674
Brian Mathews <spoonart@gmail.com>
2014-02-04 14:15:53 -0800

    refactor

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-require("./lib/nigel");
+// create http protocol
+var HTTP = require('./lib/protocols/http');
+var h = new HTTP(process.env.PORT || 3001);
+
+// create xmpp protocol
+var XMPP = require('./lib/protocols/xmpp');
+var x = new XMPP({
+    jid: 'user@gmail.com',
+    password: 'pw',
+    host: 'talk.google.com',
+    port: 5222
+});
+
+// create nigel
+var Nigel = require("./lib/nigel");
+var n = new Nigel({
+    protocols: [h,x]
+});
