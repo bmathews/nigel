fc6f0cadc0d293fa20d855f40771585f4df1d53f
stopyoukid <stopyoukid@gmail.com>
2014-02-05 16:02:12 -0800

    Added some auth, for discussion

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// create http protocol
-var HTTP = require('./lib/protocols/http');
-var h = new HTTP(process.env.PORT || 3001);
-
-// create xmpp protocol
-var XMPP = require('./lib/protocols/xmpp');
-var x = new XMPP({
-    jid: 'user@gmail.com',
-    password: 'pw',
-    host: 'talk.google.com',
-    port: 5222
-});
-
 // create nigel
 var Nigel = require("./lib/nigel");
-var n = new Nigel({
-    protocols: [h,x]
-});
+var n = new Nigel();
